c762d503b24b113b972604d60ea5beb24616b705
Kaustav Sen <sen_kaustav@hotmail.com>
2021-11-06 21:39:33 +0530

    add interactions

--- 2021_week_44/chart.js
+++ 2021_week_44/chart.js
@@ -17,7 +17,7 @@ const drawChart = async () => {
   let dimensions = {
     width: 1200,
     height: 400,
-    margin: { top: 10, right: 20, bottom: 30, left: 70 },
+    margin: { top: 10, right: 120, bottom: 30, left: 120 },
   };
 
   dimensions.boundedWidth =
@@ -154,6 +154,14 @@ const drawChart = async () => {
 
   button.on("click", changeRace);
 
+  let tooltipCircle = bounds
+    .append("circle")
+    .attr("r", 7)
+    .attr("fill", "transparent")
+    .attr("stroke", "#264653")
+    .attr("stroke-width", 1)
+    .style("opacity", 0);
+
   function changeRace() {
     counter++;
     singleRace = dataset.filter((d) => d.event_race === races[counter % 10]);
@@ -162,6 +170,82 @@ const drawChart = async () => {
     setupChart();
     subTitleEvent.text(singleRace[0].event);
     subTitleRace.text(singleRace[0].race);
+
+    tooltipCircle = bounds
+      .append("circle")
+      .attr("r", 7)
+      .attr("fill", "transparent")
+      .attr("stroke", "#264653")
+      .attr("stroke-width", 1)
+      .style("opacity", 0);
+
+    bounds
+      .selectAll("circle")
+      .on("mousemove", onMouseMove)
+      .on("mouseleave", onMouseLeave);
+  }
+
+  // Add interactions
+  const listeningRect = bounds
+    .append("rect")
+    .attr("class", "listening-rect")
+    .attr("width", dimensions.boundedWidth)
+    .attr("height", dimensions.boundedHeight)
+    .on("mousemove", onMouseMove)
+    .on("mouseleave", onMouseLeave);
+
+  bounds
+    .selectAll("circle")
+    .on("mousemove", onMouseMove)
+    .on("mouseleave", onMouseLeave);
+
+  const tooltip = d3.select("#tooltip");
+
+  function onMouseMove(event) {
+    const mousePosition = d3.pointer(event);
+    const hoveredYear = xScale.invert(mousePosition[0]);
+
+    const getDistanceFromHoveredYear = (d) =>
+      Math.abs(xAccessor(d) - hoveredYear);
+
+    const closestIndex = d3.scan(
+      singleRace,
+      (a, b) => getDistanceFromHoveredYear(a) - getDistanceFromHoveredYear(b)
+    );
+
+    const closestDataPoint = singleRace[closestIndex];
+
+    const closestXValue = xAccessor(closestDataPoint);
+    const closestYValue = yAccessor(closestDataPoint);
+
+    tooltip.select(".year").html(closestXValue);
+    tooltip.select(".time").html(timeFormat(closestYValue));
+
+    const wrapperDim = wrapper.node().getBoundingClientRect();
+
+    const x =
+      ((xScale(closestXValue) + dimensions.margin.left) * wrapperDim.width) /
+      dimensions.width;
+    const y =
+      ((yScale(closestYValue) + dimensions.margin.top) * wrapperDim.height) /
+      dimensions.height;
+
+    tooltip
+      .style("opacity", 1)
+      .style(
+        "transform",
+        `translate(calc(-50% + ${x}px), calc(-100% + ${y}px))`
+      );
+
+    tooltipCircle
+      .attr("cx", xScale(closestXValue))
+      .attr("cy", yScale(closestYValue))
+      .style("opacity", 1);
+  }
+
+  function onMouseLeave() {
+    tooltip.style("opacity", 0);
+    tooltipCircle.style("opacity", 0);
   }
 };
 drawChart();
